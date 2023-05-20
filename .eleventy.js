0202dba2e25c9ba86d59d6a1f9f7a566236bd62e
chir263 <31371125+chir263@users.noreply.github.com>
2023-05-20 16:18:44 +0530

    16th May Chirag

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/style.css");
-  eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
